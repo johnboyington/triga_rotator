901600954ca65ef8f14f256e631f1c573dad72c7
John Boyington <mjcb@ksu.edu>
2017-06-07 15:12:12 -0500

    adding rotate function

--- fuel_rotator.py
+++ fuel_rotator.py
@@ -7,6 +7,20 @@ original triga model.
 '''
 
 import numpy as np
+import math
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
 
 #read in positions from file
 F = open('input/pos_surf.txt', 'r').readlines()
@@ -21,7 +35,12 @@ cell_positions = np.array([(j[37:47], j[48:58]) for j in cell]).astype(float)
 #calculate r and theta
 cell_radii = np.sqrt(np.sum((cell_positions**2), axis=1))
 cell_theta = np.arctan(cell_positions[:,1] / cell_positions[:,0]) * (180 / np.pi)
+#for num in range(len(cell_radii)):
+#    if cell_positions[num, 1] < 0 or cell_positions[num, 1] == 0 and cell_positions[num, 0] < 0:
+#        cell_theta[num] += 180
 
+for angle in cell_theta:
+    print angle
 #input value to rotate fuel elements by
 theta_rotated = 0  #degrees counterclockwise
 
@@ -32,6 +51,10 @@ cell_new_positions = (np.array([np.cos(cell_new_theta), np.sin(cell_new_theta)])
 #calculate r and theta for surf
 surf_radii = np.sqrt(np.sum((surf_positions**2), axis=1))
 surf_theta = np.arctan(surf_positions[:,1] / surf_positions[:,0]) * (180 / np.pi)
+for num in range(len(surf_radii)):
+    if surf_positions[num, 1] < 0 or surf_positions[num, 1] == 0 and surf_positions[num, 0] < 0:
+        surf_theta[num] += 180
+
 
 #calculate new positions
 surf_new_theta = (surf_theta + theta_rotated) * (np.pi / 180)
